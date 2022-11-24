9725f0a84eaa4ab41f1fd509fb5f5a06496848f5
Steff <steff.1@gmail.com>
2022-11-24 10:55:47 +0100

    Test API

--- test.js
+++ test.js
@@ -1,6 +1,20 @@
-function test() {
-    fetch("https://webhelprequest.deta.dev/")
-        .then(async(response) => response.json())
-        .then(json => console.log(json))
+function getUsername() {
+    fetch("https://webhelprequest.deta.dev/users")
+        .then(response => response.json())
+        .then(data => console.log(data))
 }
-console.log(test());
+console.log(getUsername())
+
+function getTickets() {
+    fetch("https://webhelprequest.deta.dev/tickets")
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
+console.log(getTickets());
+
+function disableTickets() {
+    fetch("https://webhelprequest.deta.dev/tickets/1")
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
+console.log(disableTickets());
