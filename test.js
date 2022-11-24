f91644d4a3084c1250d7729c6f4bc4c83c1873de
BATARD Kevin (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC BAYONNE) <9503305e@commun.ad.sncf.fr>
2022-11-24 16:25:41 +0100

    Base du html debut du bootstrap

--- test.js
+++ test.js
@@ -1,20 +1,159 @@
-function getUsername() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function getUsername() {
     fetch("https://webhelprequest.deta.dev/users")
         .then(response => response.json())
         .then(data => console.log(data))
 }
 console.log(getUsername())
 
+
+function addTicket () {
+    const options = {
+        method:'POST',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        body: new URLSearchParams({subject:'Vraiment sympa les POST API', userId:'b23xgcuky9pt'})};
+    fetch('https://webhelprequest.deta.dev/tickets', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+    }
+
+
 function getTickets() {
     fetch("https://webhelprequest.deta.dev/tickets")
         .then(response => response.json())
         .then(data => console.log(data))
 }
-console.log(getTickets());
+console.log(getTickets()); 
 
+ */
+
+/* 
 function disableTickets() {
     fetch("https://webhelprequest.deta.dev/tickets/1")
         .then(response => response.json())
         .then(data => console.log(data))
 }
-console.log(disableTickets());
+console.log(disableTickets()); */
+
+
+/*const options = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+    body: new URLSearchParams({username: 'kev', password: '1234'})
+  };
+  
+  fetch('https://webhelprequest.deta.dev/users', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));*/
+
+
